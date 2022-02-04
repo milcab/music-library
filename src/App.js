bc10f5e72ce8bb5af3a15414c47702bfb2de63cb
milcab <milcarodriguez23@gmail.com>
2022-02-04 14:10:36 -0500

    fixing the app

--- src/App.js
+++ src/App.js
@@ -8,13 +8,38 @@ function App() {
   let [message, setMessage] = useState('Search for Music!')
   let [data, setData] = useState([])
 
+  const API_URL = 'https://itunes.apple.com/search?term=the%20grateful%20dead'
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      document.title = `${search} Music`
+      const response = await fetch(API_URL + search)
+      const resData = await response.json()
+      console.log(resData)
+      if(resData.results.length > 0) {
+        setData(resData.results)
+      } else {
+        setMessage('Not Found')
+      }
+    }
+    fetchData()
+  }, [search])
+
+  // App.js
+  const handleSearch = (e, term) => {
+    e.preventDefault()
+    setSearch(term)
+  }
+
   return (
     <div>
-      <Searchbar />
+      <Searchbar handleSearch={handleSearch} />
       {message}
-      <Gallery />
+      <Gallery data={data} />
     </div>
   )
+
 }
+
 
 export default App
--- src/components/gallery.js
+++ src/components/gallery.js
@@ -1,10 +1,18 @@
 // Gallery.js
+// Gallery.js
 import GalleryItem from './GalleryItem'
 
 function Gallery(props) {
+
+    const display = props.data.map((item, index) => {
+        return (
+            <GalleryItem item={item} key={index} />
+        )
+    })
+
     return (
         <div>
-            <GalleryItem />
+            {display}
         </div>
     )
 }
